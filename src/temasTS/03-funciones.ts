6933f03083f257ed2be67db8b06637a65fbf64c3
Matilde Blas <blasmati619@gmail.com>
2025-09-10 20:55:18 -0600

    Importar exportar

--- src/temasTS/03-funciones.ts
+++ src/temasTS/03-funciones.ts
@@ -16,3 +16,28 @@ console.log(multiplicar(3,5,4));
 //funciones tipo flecha
 const addNumberArrow=(a:number, b:number):string=>{return `${a+b}`} //`${a+b}` permite imprimir los datos en string 
 
+interface Mascotas{
+    nombre:string,
+    edad: number,
+    raza: string,
+    sexo?:string,
+    vacunado:boolean
+    mostrarEdad:()=>void
+}
+
+function mostrarMascota(mascota:Mascotas, x:number):void{
+    mascota.edad+x;
+    console.log(mascota);
+    console.log(mascota.mostrarEdad());
+}
+const nuevaMascota:Mascotas={
+    nombre:"Filurais",
+    edad:5,
+    raza: "pastor aleman",
+    vacunado:true,
+    mostrarEdad(){
+        console.log(`la edad de ${this.nombre} es ${this.edad}`);
+    }
+
+}
+mostrarMascota(nuevaMascota,2);
